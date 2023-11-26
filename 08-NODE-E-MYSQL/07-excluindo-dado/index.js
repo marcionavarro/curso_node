51dd2d213fe1777a64f08426be2d3840bb3a991d
marcionavarro <marcionavarrodearaujo@gmail.com>
2023-11-26 12:17:13 -0300

    08-NODE-E-MYSQL - Complete

---
+++ 08-NODE-E-MYSQL/07-excluindo-dado/index.js
@@ -0,0 +1,127 @@
+const express = require("express");
+const { engine } = require("express-handlebars");
+const mysql = require("mysql2");
+
+const app = express();
+const log = console.log;
+
+app.use(
+  express.urlencoded({
+    extended: true
+  })
+);
+app.use(express.json());
+
+app.engine("handlebars", engine());
+app.set("view engine", "handlebars");
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.post("/books/insertbook", (req, res) => {
+  const title = req.body.title;
+  const pageqty = req.body.pageqty;
+
+  const sql = `INSERT INTO books (title, pageqty) VALUES ('${title}', '${pageqty}')`;
+  conn.query(sql, function (err) {
+    if (err) {
+      log(err);
+      return;
+    }
+
+    res.redirect("/books");
+  });
+});
+
+app.get("/books", (req, res) => {
+  const sql = "SELECT * FROM books";
+  conn.query(sql, function (err, data) {
+    if (err) {
+      log(err);
+      return;
+    }
+    const books = data;
+    log(books);
+    res.render("books", { books });
+  });
+});
+
+app.get("/books/:id", (req, res) => {
+  const id = req.params.id;
+  const sql = `SELECT * FROM books WHERE id = ${id}`;
+  conn.query(sql, function (err, data) {
+    if (err) {
+      log(err);
+      return;
+    }
+
+    const book = data[0];
+    res.render("book", { book });
+  });
+});
+
+app.get("/books/edit/:id", (req, res) => {
+  const id = req.params.id;
+  const sql = `SELECT * FROM books WHERE id = ${id}`;
+  conn.query(sql, function (err, data) {
+    if (err) {
+      log(err);
+      return;
+    }
+
+    const book = data[0];
+    res.render("editbook", { book });
+  });
+});
+
+app.post("/books/updatebook", (req, res) => {
+  const id = req.body.id;
+  const title = req.body.title;
+  const pageqty = req.body.pageqty;
+  pageqty;
+
+  const sql = `UPDATE books SET title = '${title}', pageqty = '${pageqty}'  WHERE id = '${id}'`;
+
+  conn.query(sql, function (err) {
+    if (err) {
+      console.log(err);
+      return;
+    }
+
+    res.redirect("/books");
+  });
+});
+
+app.post("/books/remove/:id", (req, res) => {
+  const id = req.params.id;
+
+  const sql = `DELETE FROM books WHERE id = '${id}'`;
+
+  conn.query(sql, function (err) {
+    if (err) {
+      console.log(err);
+      return;
+    }
+
+    res.redirect("/books");
+  });
+
+})
+
+const conn = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "admin123",
+  database: "nodemysql"
+});
+
+conn.connect(function (err) {
+  if (err) {
+    log(err);
+  }
+  log("Conectou ao MySQL!");
+  app.listen(3000);
+});
