852d7df0ce6676b4d65e9efbd1ec3a796e1d9bb9
marcionavarro <marcionavarrodearaujo@gmail.com>
2023-11-27 22:18:14 -0300

    09-SEQUELIZE - criando-model, inserindo-dado, lendo-dados, resgatando-dado

--- 09-SEQUELIZE/02-criando-model/index.js
+++ 09-SEQUELIZE/02-criando-model/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const { engine } = require("express-handlebars");
-const pool = require("./db/conn");
+const conn = require("./db/conn");
+
+const User = require("./models/User");
 
 const app = express();
 const log = console.log;
@@ -21,4 +23,7 @@ app.get("/", (req, res) => {
   res.render("home");
 });
 
-app.listen(3000);
+conn.sync().then(() => {
+  app.listen(3000);
+}).catch((err) => log(err))
+
