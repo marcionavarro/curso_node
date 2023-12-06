bbebf7f2b7541e1464d17a1772a73e2c37bb814e
marcionavarro <marcionavarrodearaujo@gmail.com>
2023-12-05 23:23:05 -0300

    10-MVC - Model, Controller, Rotas

--- 10-MVC/03-controller/index.js
+++ 10-MVC/03-controller/index.js
@@ -5,6 +5,8 @@ const app = express();
 
 const conn = require("./db/conn");
 
+const Task = require("./models/Task");
+
 app.engine("handlebars", engine());
 app.set("view engine", "handlebars");
 
@@ -18,6 +20,11 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-app.listen(3000, () => {
-  console.log('http://localhost:3000')
-});
+conn
+  .sync()
+  .then(() => {
+    app.listen(3000, () => {
+      console.log("http://localhost:3000");
+    });
+  })
+  .catch((error) => console.log(error));
--- 10-MVC/04-rotas/index.js
+++ 10-MVC/04-rotas/index.js
@@ -5,6 +5,9 @@ const app = express();
 
 const conn = require("./db/conn");
 
+const Task = require("./models/Task");
+const tasksRoutes = require("./routes/tasksRoutes");
+
 app.engine("handlebars", engine());
 app.set("view engine", "handlebars");
 
@@ -15,9 +18,15 @@ app.use(
 );
 
 app.use(express.json());
-
 app.use(express.static("public"));
 
-app.listen(3000, () => {
-  console.log('http://localhost:3000')
-});
+app.use("/tasks", tasksRoutes);
+
+conn
+  .sync()
+  .then(() => {
+    app.listen(3000, () => {
+      console.log("http://localhost:3000");
+    });
+  })
+  .catch((error) => console.log(error));
